dafa08b2fb7613227de493ab33171fdc0e4c254a
Lennart Melzer <lennart@melzer.it>
2018-10-04 13:32:07 +0200

    handle primitive messages from sc

--- src/tmpaa.cpp
+++ src/tmpaa.cpp
@@ -1,17 +1,30 @@
 #include <Arduino.h>
 
-// protocol handling
-String paramBuffer = "";
-String nodeName = "dcarm";
+#define MOTOR_EN_A 11
+#define MOTOR_IN1 9
+#define MOTOR_IN2 8
+#define MOTOR_IN3 7
+#define MOTOR_IN4 6
+#define MOTOR_EN_B 10
 
 #define READ_NEWLINE_NOW 0
 #define READ_DEVICE_NOW 1
 #define READ_N_NOW 2
 
+#define LED_INSTANCE 0
+#define MOTOR_INSTANCE 1
 #define NUM_INSTANCES 2
 
+typedef int instance_type;
+
+instance_type instances[NUM_INSTANCES] = { LED_INSTANCE, MOTOR_INSTANCE };
+
+// protocol handling
+String paramBuffer = "";
+String nodeName = "dcarm";
+
 typedef struct {
-  bool stage;
+  int stage;
 } parse_state;
 
 parse_state parserState = { READ_NEWLINE_NOW };
@@ -31,16 +44,27 @@ led_state targetState = { false, millis() };
 /* e.g. for a attached motor */
 typedef struct {
   float speed;
+  bool forward;
   unsigned long changedAt;
 } motor_state;
 
-motor_state currentMotorState = { false, millis() };
-motor_state targetMotorState = { false, millis() };
+motor_state currentMotorState = { 0.0, true, millis() };
+motor_state targetMotorState = { 0.0, true, millis() };
+
+
 
 void setup() {
   digitalWrite(13, LOW); // LED13 initially off
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
+
+  pinMode(MOTOR_EN_A, OUTPUT);  
+  pinMode(MOTOR_IN1, OUTPUT);
+  pinMode(MOTOR_IN2, OUTPUT);
+  pinMode(MOTOR_IN3, OUTPUT);
+  pinMode(MOTOR_IN4, OUTPUT);
+  pinMode(MOTOR_EN_B, OUTPUT);  
+  
   digitalWrite(2, LOW);  
   Serial.begin(9600);
 }
@@ -56,41 +80,59 @@ void loop() {
   if (currentState.on && ((now - currentState.changedAt) >= defaultDuration)) {
     targetState.on = false;
   }
+
+  if (targetMotorState.speed != currentMotorState.speed) {
+    currentMotorState.speed = targetMotorState.speed;
+    currentMotorState.changedAt = millis();
+    digitalWrite(MOTOR_IN1, LOW);
+    digitalWrite(MOTOR_IN2, HIGH);
+    analogWrite(MOTOR_EN_A, floor(currentMotorState.speed * 255));
+  }
+
+  if ((currentMotorState.speed > 0) && ((now - currentMotorState.changedAt) >= defaultDuration)) {
+    digitalWrite(MOTOR_IN1, LOW);
+    digitalWrite(MOTOR_IN2, LOW);
+    analogWrite(MOTOR_EN_A, 0);
+    targetMotorState.speed = 0.0;
+  }
 }
 
 void serialEvent() {
   while(Serial.available() > 0) {
     char currentChar = (char)Serial.read();
-
-    if (currentChar == '\n') {
-      // full message received
-      parserState.stage = READ_DEVICE_NOW;
-      paramBuffer = "";
-    }
-    else if (parserState.stage == READ_DEVICE_NOW) {
-      if (currentChar == ' ') {
-        if (parserState.stage == READ_N_NOW) {
-            // read int
-          //paramBuffer.toInt() % NUM_INSTANCES;
-          targetState.on = true;
-          parserState.stage = READ_NEWLINE_NOW;
-        }
-        else {
-          if (paramBuffer == nodeName) {
-            // this message is for us
-            parserState.stage = READ_N_NOW;
-          }
-        }
+    switch (parserState.stage) {
+    case READ_DEVICE_NOW:
+      if ((currentChar == ' ') && (paramBuffer == nodeName)) {
+        parserState.stage = READ_N_NOW;
+        paramBuffer = "";
       }
       else {
         // buffering
-        if (parserState.stage != READ_NEWLINE_NOW) {
-          paramBuffer += currentChar;
-        }
+        paramBuffer += currentChar;
       }
-    }
-    else {
-      // skipping until newline
+      break;
+    case READ_NEWLINE_NOW:
+      if (currentChar == '\n') {
+        // full message received
+        parserState.stage = READ_DEVICE_NOW;
+        paramBuffer = "";
+      }
+      break;
+    case READ_N_NOW:
+      if (currentChar == ' ') {
+        if (paramBuffer.toInt() == 0) {
+          targetMotorState.speed = 1.0;
+        }
+        else {
+          targetState.on = true;              
+        }
+        parserState.stage = READ_NEWLINE_NOW;
+      }
+      else {
+        // buffering
+        paramBuffer += currentChar;
+      }
+      break;
     }
   }
 }
